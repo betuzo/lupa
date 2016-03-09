357824f5011c493c451df1dbc9334e99ad646126
rolguin <rolguin@bursatec.com.mx>
2016-03-09 13:05:01 -0600

    cambiando propiedades

--- repositories/src/main/resources/com/codigoartesanal/lupa/scripts/h2/ddl/ingresototales.sql
+++ repositories/src/main/resources/com/codigoartesanal/lupa/scripts/h2/ddl/ingresototales.sql
@@ -3,7 +3,7 @@ DROP TABLE IF EXISTS Ingresos_Totales_View;
 Create view Ingresos_Totales_View as
 
 select
-	1 as id,
+	1 as id, --row_number() over() as id, postgresql
 	SUM(CASE WHEN status='REGISTRADA' THEN 1 ELSE 0 END) as totalPendientes,
 	sum(monto) as totalMontoPendiente,
 	SUM(CASE WHEN status='VALIDA' THEN monto ELSE 0 END) as totalMonto
